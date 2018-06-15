5659adf77939f952a668467812f8e4517ce25c18
TJRobson <thomas.j.robson@hotmail.com>
2018-06-15 23:30:53 +1200

    Chapter15 dir and exercise 1

---
+++ chapter15/exercises/chap15ex1.c
@@ -0,0 +1,15 @@
+/* Chapter 15 Exercises 1a 1b */
+
+/* Section 15.1 listed several advantages of dividing a program into multiple
+ * source files.
+ *
+ * a) Describe several other advantages.
+ *
+ * - Less to compiling to when changes are made.
+ * - Smaller source files are easier to test and reason with.
+ *
+ * b) Describe some disadvantages.
+ *
+ * - Needlessly Complicates simple programs.
+ * -
+*/
