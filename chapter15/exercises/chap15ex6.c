9b25d693ee13f2173e357b2b431fdde7024f5796
TJRobson <thomas.j.robson@hotmail.com>
2018-06-28 17:42:39 +1200

    Chapter 15 Exercise 6

---
+++ chapter15/exercises/chap15ex6.c
@@ -0,0 +1,13 @@
+/* Chapter 15 Exericses 6a-6d */
+
+/* The following questions refer to the program described in Exercise 5
+ *
+ * a) Which files need to be complied when the program is built for the
+ *    first time? Answer: main.c f1.c f2.c
+ * b) If f1.c is changed after the program has been built, which files need
+ *    to be recompiled? Answer: f1.c
+ * c) If f1.h is changed after the program has been built, which files need
+ *    to be recompiled? Anser: main.c f1.c f2.c
+ * d) If f2.h is changed after the program has been built, which files need
+ *    to be recompiled? Answer: f1.c f2.c
+*/
