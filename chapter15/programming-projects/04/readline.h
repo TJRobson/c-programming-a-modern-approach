9ecf3b9005420732e62b4fa7cbf7d3f8e129b26d
TJRobson <thomas.j.robson@hotmail.com>
2018-07-05 23:37:00 +1200

    Chapter 15, Programing project 4, reaminder program files - all working

---
+++ chapter15/programming-projects/04/readline.h
@@ -0,0 +1,9 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+/**********************************************************
+ * Reads in a sentence and stores it as a string          *
+ **********************************************************/
+int read_line(char str[], int n);
+
+#endif
