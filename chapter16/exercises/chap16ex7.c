8c0c4a10d76d310c60dde90f8a3ab1e93662b2e9
TJRobson <thomas.j.robson@hotmail.com>
2018-07-16 22:59:35 +1200

    Exercise 7, setting up file and tasks.

---
+++ chapter16/exercises/chap16ex7.c
@@ -0,0 +1,39 @@
+/* Chapter 16 Exercise 7 */
+#include <stdio.h>
+
+struct fraction {
+  int numerator;
+  int demoninator;
+};
+
+struct fraction gcd(struct f);                        /* 7a */
+struct fraction add_fractions(struct f1, struct f2);  /* 7b */
+struct fraction sub_fractions(struct f1, struct f2);  /* 7c */
+struct fraction mult_fractions(struct f1, struct f2); /* 7d */
+struct fraction divd_fractions(struct f1, struct f2); /* 7e */
+
+int main(void)
+{
+  return 0;
+}
+
+struct fraction gcd(struct f)
+{
+
+}
+struct fraction add_fractions(struct f1, struct f2)
+{
+
+}
+struct fraction sub_fractions(struct f1, struct f2)
+{
+
+}
+struct fraction mult_fractions(struct f1, struct f2)
+{
+
+}
+struct fraction divd_fractions(struct f1, struct f2)
+{
+
+}
