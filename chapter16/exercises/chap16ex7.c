68c6bd59f9ec0b22634420626393447feb753bf9
TJRobson <thomas.j.robson@hotmail.com>
2018-07-18 00:36:00 +1200

    Exercise 7, started completing exercises.

--- chapter16/exercises/chap16ex7.c
+++ chapter16/exercises/chap16ex7.c
@@ -6,34 +6,53 @@ struct fraction {
   int demoninator;
 };
 
-struct fraction gcd(struct f);                        /* 7a */
-struct fraction add_fractions(struct f1, struct f2);  /* 7b */
-struct fraction sub_fractions(struct f1, struct f2);  /* 7c */
-struct fraction mult_fractions(struct f1, struct f2); /* 7d */
-struct fraction divd_fractions(struct f1, struct f2); /* 7e */
+struct fraction reduce(struct fraction f);
+int fraction gcd(int one, int two); /* 7a */
+
+struct fraction add_fractions(struct fraction f1, struct fraction f2); /* 7b */
+struct fraction sub_fractions(struct fraction f1, struct fraction f2); /* 7c */
+struct fraction mult_fractions(struct fraction f1, struct fraction f2);/* 7d */
+struct fraction divd_fractions(struct fraction f1, struct fraction f2);/* 7e */
 
 int main(void)
 {
   return 0;
 }
 
-struct fraction gcd(struct f)
+struct fraction reduce(struct fraction f)
+{
+  int gcd = gcd(f.numerator, f.demoninator);
+  f.numerator /= gcd;
+  f.demoninator /= gcd;
+  return f;
+}
+
+struct fraction gcd(int one, int two)
+{
+  int temp;
+
+  while (one != 0) {
+    temp = two % one;
+    two = one;
+    one = temp;
+  }
+  return two;
+}
+
+struct fraction add_fractions(struct fraction f1, struct fraction f2)
+{
+
+
+}
+struct fraction sub_fractions(struct fraction f1, struct fraction f2)
 {
 
 }
-struct fraction add_fractions(struct f1, struct f2)
+struct fraction mult_fractions(struct fraction f1, struct fraction f2)
 {
 
 }
-struct fraction sub_fractions(struct f1, struct f2)
-{
-
-}
-struct fraction mult_fractions(struct f1, struct f2)
-{
-
-}
-struct fraction divd_fractions(struct f1, struct f2)
+struct fraction divd_fractions(struct fraction f1, struct fraction f2)
 {
 
 }
