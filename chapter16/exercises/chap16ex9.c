9ef237c0a2f51c3ad15b0da8a4ca4ce7749a1bb4
TJRobson <thomas.j.robson@hotmail.com>
2018-07-23 22:25:21 +1200

    Exercise 9, setting up file.

---
+++ chapter16/exercises/chap16ex9.c
@@ -0,0 +1,45 @@
+/* Chapter 16 Exercise 9a-e */
+#include <stdio.h>
+#include <stdbool.h>
+
+struct colour {
+  int red;
+  int green;
+  int blue;
+};
+
+struct colour make_colour(int red, int green, int blue);
+int getRed(struct colour c);
+bool equal_colour(struct colour colour1, struct colour colour2);
+struct colour brightner(struct colour c);
+struct colour darkner(struct colour c);
+
+int main(void)
+{
+  return 0;
+}
+
+struct colour make_colour(int red, int green, int blue)
+{
+
+}
+
+int getRed(struct colour c)
+{
+
+}
+
+bool equal_colour(struct colour colour1, struct colour colour2)
+{
+
+}
+
+struct colour brightner(struct colour c)
+{
+
+}
+
+struct colour darkner(struct colour c)
+{
+  
+}
