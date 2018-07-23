d280a8d601de89ee60061c694333afec38774eb0
TJRobson <thomas.j.robson@hotmail.com>
2018-07-23 23:17:56 +1200

    Exercise 9d, colour brightner.

--- chapter16/exercises/chap16ex9.c
+++ chapter16/exercises/chap16ex9.c
@@ -21,7 +21,7 @@ int main(void)
 
 struct colour make_colour(int red, int green, int blue)
 {
-  struct colour c {.red = red, .green = green, .blue = blue};
+  struct colour c  = {.red = red, .green = green, .blue = blue};
 
   if (c.red < 0) {
     c.red = 0;
@@ -48,6 +48,37 @@ bool equal_colour(struct colour c1, struct colour c2)
 struct colour brightner(struct colour c)
 {
 
+  if (c.red == 0 && c.green == 0 && c.blue == 0) {
+    struct colour brightner = {3, 3, 3};
+    return brightner;
+  }
+
+  struct colour brighter;
+
+  if (c.red > 0 && c.red < 3) {
+    c.red = 3;
+  }
+  if (c.green > 0 && c.green < 3) {
+    c.green = 3;
+  }
+  if (c.blue > 0 && c.blue < 3) {
+    c.blue = 3;
+  }
+
+  brighter.red = (int) (c.red / 0.7);
+  if (brighter.red > 255) {
+    brighter.red = 255;
+  }
+  brighter.green = (int) (c.green / 0.7);
+  if (brighter.green > 255) {
+    brighter.green = 255;
+  }
+  brighter.blue = (int) (c.blue / 0.7);
+  if (brighter.blue > 255) {
+    brighter.blue = 255;
+  }
+
+  return brighter;
 }
 
 struct colour darkner(struct colour c)
