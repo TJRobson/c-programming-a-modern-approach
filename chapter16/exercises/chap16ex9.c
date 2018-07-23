6a61d0dfc2a1ef8fe9ebfb2c47d627bc057e0ec3
TJRobson <thomas.j.robson@hotmail.com>
2018-07-23 22:35:53 +1200

    Exercise 9a, make_colour.

--- chapter16/exercises/chap16ex9.c
+++ chapter16/exercises/chap16ex9.c
@@ -21,7 +21,18 @@ int main(void)
 
 struct colour make_colour(int red, int green, int blue)
 {
+  struct colour c {.red = red, .green = green, .blue = blue};
 
+  if (c.red < 0) {
+    c.red = 0;
+  }
+  if (c.green < 0) {
+    c.green = 0;
+  }
+  if (c.blue < 0) {
+    c.blue = 0;
+  }
+  return c;
 }
 
 int getRed(struct colour c)
@@ -41,5 +52,5 @@ struct colour brightner(struct colour c)
 
 struct colour darkner(struct colour c)
 {
-  
+
 }
