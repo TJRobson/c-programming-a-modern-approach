2ed5b65b7be69102f71ad2f217395c0f2417dcfd
TJRobson <thomas.j.robson@hotmail.com>
2018-07-28 22:38:02 +1200

    Exercise 10 finished.

--- chapter16/exercises/chap16ex10.c
+++ chapter16/exercises/chap16ex10.c
@@ -15,6 +15,41 @@ bool contains_point(struct rectangle r, struct point p);
 
 int main(void)
 {
+  struct point p1, p2;
+
+  printf("\nEnter an x, y pair for point 1 (x, y): ");
+  scanf("%d, %d", &p1.x, &p1.y);
+  printf("Enter an x, y pair for point 1 (x, y): ");
+  scanf("%d, %d", &p2.x, &p2.y);
+
+  struct rectangle r = {p1, p2};
+  struct point center_of_r = find_center(r);
+
+  printf("\nArea of rectangle: %d\n", compute_area(r));
+  printf("Center of rectangle: x = %d, y = %d\n",
+          center_of_r.x, center_of_r.y);
+
+  int shift_x, shift_y;
+
+  printf("Enter x direction shift amount (int): ");
+  scanf("%d", &shift_x);
+  printf("Enter y direction shift amount (int): ");
+  scanf("%d", &shift_y);
+
+  r = shift_rectangle(r, shift_x, shift_y);
+  center_of_r = find_center(r);
+
+  printf("\nCenter of shifted rectangle: x = %d, y = %d",
+         center_of_r.x, center_of_r.y);
+
+  struct point is_point;
+
+  printf("\nEnter a x, y to check for in rectangle (x, y): ");
+  scanf("%d, %d", &is_point.x, &is_point.y);
+
+  printf("Point is in rectangle: %s\n",
+         contains_point(r, is_point) ? "True":"False");
+
 
   return 0;
 }
@@ -36,12 +71,8 @@ int compute_area(struct rectangle r)
 
 struct point find_center(struct rectangle r)
 {
-  struct point center;
-
-  center.x = r.lower_right.x + (compute_width(r) / 2);
-  center.y = r.lower_right.y + (compute_height(r) / 2);
-
-  return center;
+  return (struct point) {r.lower_right.x + (compute_width(r) / 2),
+                         r.lower_right.y + (compute_height(r) / 2)};
 }
 
 struct rectangle shift_rectangle(struct rectangle r, int x, int y)
