1b884e8ff71ac1b95943678ed0d4efd5902c39b7
TJRobson <thomas.j.robson@hotmail.com>
2018-07-29 22:29:15 +1200

    Exercise 14a-c, setting up file. Completed 14a.

---
+++ chapter16/exercises/chap16ex14.c
@@ -0,0 +1,41 @@
+/* Chapter 16 Exercise 14 */
+#include <stdio.h>
+#include <math.h>
+
+#define M_PI acos(-1.0)
+#define RECTANGLE 0
+#define CIRCLE 1
+
+struct point { int x, y; };
+
+struct shape {
+  int shape_kind;       /* RECTANGLE  or CIRCLE */
+  struct point center;  /* coordinates of center */
+  union {
+    struct {
+      int height, width;
+    } rectangle;
+    struct {
+      int radius;
+    } circle;
+  } u;
+} s;
+
+int compute_area(struct shape s);
+struct shape move_shape(struct shape s, int x, int y);
+struct shape scale_shape(struct shape s, double c);
+
+int main(void)
+{
+
+  return 0;
+}
+
+int compute_area(struct shape s)
+{
+  if (s.shape_kind == RECTANGLE) {
+    return s.u.rectangle.height * s.u.rectangle.height;
+  } else {
+    return (int) M_PI * (s.u.circle.radius * s.u.circle.radius);
+  }
+}
