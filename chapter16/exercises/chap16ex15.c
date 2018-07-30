42b2996dbaefc3a5bbd059146ec4e4fda264261f
TJRobson <thomas.j.robson@hotmail.com>
2018-07-30 23:16:20 +1200

    Exercise 15 a/b

---
+++ chapter16/exercises/chap16ex15.c
@@ -0,0 +1,11 @@
+/* Chapter 16 Exercise 15 */
+
+/* a) Declear a tag for an enumeration whose values represent the days
+ * of the week.
+*/
+
+enum day {MON, TUE, WED, THU, FRI, SAT, SUN};
+
+/* b) Use a typedef to define a name for the enumeration of part a). */
+
+typedef enum {MON, TUE, WED, THU, FRI, SAT, SUN} Day;
