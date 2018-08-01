8b0df2e16e4dc5c551903e1dc3c242dba5f10dd8
TJRobson <thomas.j.robson@hotmail.com>
2018-08-01 23:16:14 +1200

    Exercises 21 a-d completed.

---
+++ chapter16/exercises/chap16ex21.c
@@ -0,0 +1,21 @@
+/* Chapter 16 Exercise 21 */
+
+/* What are the integer values of the enumerations constants in each
+ * of the following declarations?
+*/
+
+/* a) */
+enum {NUL, SOH, STX, ETX};
+/* Answer: 0, 1, 2, 3 */
+
+/* b) */
+enum {VT = 11, FF, CR};
+/* Answer: 11, 12, 13 */
+
+/* c) */
+enum {SO = 14, SI, DLE, CAN = 24, EM}
+/* Answer: 14, 15, 16, 24, 25 */
+
+/* d) */
+enum {ENQ = 45, ACK, BEL, LF = 37, ETB, ESC}
+/* Answer: 45, 46, 47, 37, 38, 39 */
