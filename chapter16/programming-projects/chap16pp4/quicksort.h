71ea47f03c2714ece1717c24855c6f2ab650f712
TJRobson <thomas.j.robson@hotmail.com>
2018-08-13 22:45:25 +1200

    Programming project 4 completed, price added to part struct, with supporting funcs

---
+++ chapter16/programming-projects/chap16pp4/quicksort.h
@@ -0,0 +1,10 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+#include "inventory.h"
+/***********************************************************
+ * Sorts the integer array a using the quicksort algorithm *
+ ***********************************************************/
+void quicksort(struct part a[], int low, int high);
+
+#endif
