5f5520d885435f291770e024978ff3a7bbbb8ac0
TJRobson <thomas.j.robson@hotmail.com>
2018-09-21 14:01:50 +1200

    Wrong code example, not working yet

---
+++ chapter17/programming-projects/chap17pp7-wrong-one.c
@@ -0,0 +1,102 @@
+/* Chapter 17 Programming Project 7 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_MSGS 50
+#define MSG_MAX 20
+
+struct vstring {
+  int len;
+  char chars[];
+};
+
+void *my_malloc(int bytes);
+int read_line(char *str, int n);
+int compare_words(const void *p, const void *q);
+void sort_and_print(struct vstring **pp, int n);
+
+int main(void)
+{
+  char *msg_str;
+  struct vstring **msgs, *msg_vstring;
+  int size = 1, num_msgs = 0;
+
+  printf("\n");
+  msgs = my_malloc(sizeof(struct vstring *));
+
+  for (; num_msgs < MSG_MAX; ) {
+
+    msg_str = my_malloc(MSG_MAX + 1);
+
+    printf("Enter a message: ");
+    read_line(msg_str, MSG_MAX);
+
+    if (strcmp(msg_str, "") == 0) {
+      break;
+    }
+
+    msg_vstring = my_malloc(sizeof(struct vstring) +
+                            2 + strlen(msg_str) + 1);
+
+    strcpy(msg_vstring->chars, msg_str);
+    msg_vstring->len = strlen(msg_str);
+
+    *(msgs + num_msgs) = msg_vstring;
+
+    num_msgs++;
+
+    if (num_msgs == size) {
+      msgs = realloc(msgs, sizeof(struct vstring *) * (size *= 2));
+    }
+  }
+
+  sort_and_print(msgs, num_msgs);
+
+  return 0;
+}
+
+void *my_malloc(int bytes)
+{
+  void *p;
+
+  if ((p = malloc(bytes)) == NULL) {
+    printf("\nError: NULL pointer, malloc failed.\n");
+    exit(EXIT_FAILURE);
+  }
+  return p;
+}
+
+int read_line(char *str, int n)
+{
+  char *p = str;
+
+  while ((*p = getchar()) != '\n'){
+    if (p < str + n) {
+      p++;
+    }
+  }
+
+  if (p[strlen(p)-1] == '\n') {
+    p[strlen(p)-1] = '\0';
+  }
+
+  return p - str;
+}
+
+int compare_words(const void *p, const void *q)
+{
+  return strcmp( *(char **)p, *(char **)q );
+}
+
+void sort_and_print(struct vstring **pp, int n)
+{
+  // qsort(pp, n, sizeof(char *), compare_words);
+  printf("\nSorted Messages:");
+
+  int i;
+  for (i = 0; i < n; i++) {
+    printf("\n%d | %s", i, (*pp + i)->chars);
+  }
+  printf("\n");
+}
