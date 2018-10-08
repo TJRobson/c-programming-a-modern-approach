af8b7f56d27a14ce87a78bb68360c34e2629e6a8
TJRobson <thomas.j.robson@hotmail.com>
2018-10-08 22:56:53 +1300

    Exercise 7

---
+++ chapter18/exercises/chap18ex7.c
@@ -0,0 +1,14 @@
+/* Chapter 18 Exercise 7 */
+
+/* Suppose that we declare x to be a const object. Which of the following
+ * statements about x false?
+ *
+ * a) If x is of type int, it can be used as the value of a case label in
+ *    a switch statement.
+ *    False - a const variable is not a constant expression.
+ *
+ * b) The compiler will check that no assignment is made to x. True.
+ * c) x is subject to the same scope rules as variables. True.
+ * d) x can be of any type. True.
+ *
+*/
