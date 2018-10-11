238922169afcbf3ce4471bc9dd5925107faf7561
TJRobson <thomas.j.robson@hotmail.com>
2018-10-12 11:55:33 +1300

    Exercise 14

---
+++ chapter18/exercises/chap18ex14.c
@@ -0,0 +1,12 @@
+/* Chapter 18 Exercise 14 */
+
+/* Which kind of variables cannot be initialized */
+
+/* a) Array variables
+ * b) Enumeration variables
+ * c) Structure variables
+ * d) Union variables
+ * e) None of the above
+*/
+
+/* Answer: e, none of the above. */
