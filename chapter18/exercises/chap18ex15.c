e297ff96a60cdb99517b4cd2f308517acf903f46
TJRobson <thomas.j.robson@hotmail.com>
2018-10-12 12:00:58 +1300

    Exercise 15, chapter 18 completed

---
+++ chapter18/exercises/chap18ex15.c
@@ -0,0 +1,17 @@
+/* Chapter 18 Exericse 15 */
+
+/* Which property of a variable determines whether or not it has a default
+ * initial value?
+*/
+
+/* a) Storage duration
+ * b) Scope
+ * c) Linkage
+ * d) Type
+*/
+
+/* Answer: a, storage duration.
+ * A variable with static storage duration will have a default zero value,
+ * corresponding to the type of the variable (e.g. int will be 0, float 0.0,
+ * and pointers will be NULL.)
+*/
