1a7eb6faa24157906e10461e5d1f5f54c6b02265
TJRobson <thomas.j.robson@hotmail.com>
2018-11-20 15:07:04 +1300

    queue functions complete

---
+++ chapter19/exercises/03/b/queue.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+#define PUBLIC
+#define PRIVATE static
+
+PRIVATE void queue_underflow(void);
+
+PRIVATE struct node {
+  Item item;
+  struct node *next;
+} *head = NULL, *tail = NULL;
+
+PRIVATE int num_items = 0;
+
+PUBLIC void end_insert(Item i)
+{
+  struct node *new_node;
+
+  if ((new_node = malloc(sizeof(struct node))) == NULL) {
+    printf("\nError, malloc failed in end_insert.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  new_node->item = i;
+  new_node->next = NULL;
+  num_items++;
+
+  if (head == NULL) {
+    head = tail = new_node;
+  } else {
+    tail->next = new_node;
+    tail = new_node;
+  }
+}
+
+PUBLIC Item dequeue(void)
+{
+  if (!is_empty())
+  {
+    struct node *temp = head;
+    Item i = head->item;
+    head = head->next;
+    free(temp);
+    num_items--;
+    return i;
+  }
+  else
+  {
+    queue_underflow();
+  }
+}
+
+PUBLIC Item get_first(void)
+{
+  if (!is_empty()) {
+    return head->item;
+  }
+  else {
+    queue_underflow();
+  }
+}
+
+PUBLIC Item get_last(void)
+{
+  if (!is_empty()) {
+    return tail->item;
+  }
+  else {
+    queue_underflow();
+  }
+}
+
+PUBLIC int items_in_queue(void)
+{
+  return num_items;
+}
+
+PUBLIC bool is_empty(void)
+{
+  return head == NULL;
+}
+
+PRIVATE void queue_underflow(void)
+{
+  printf("\nError, queue is empty.\n");
+  exit(EXIT_FAILURE);
+}
