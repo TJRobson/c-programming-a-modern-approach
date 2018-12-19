8dd51f3dc5c1801c659ed2259767452e98dbdd34
TJRobson <thomas.j.robson@hotmail.com>
2018-12-19 14:30:06 +1300

    Exercise 6b done.

--- chapter20/exercises/chap20ex6.c
+++ chapter20/exercises/chap20ex6.c
@@ -1,7 +1,11 @@
-/* Chapter 20 Exercises 6a-6b */
+/* Chapter 20 Exercises 6 */
 #include <stdio.h>
 
+/* Exercise 6a) */
 unsigned short swap_bytes(unsigned short i);
+
+/* Exercise 6b */
+unsigned short swap_bytes_b(unsigned short i);
 
 int main(void)
 {
@@ -9,7 +13,7 @@ int main(void)
 
   printf("\nEnter a hexadecimal number: ");
   scanf("%hx", &hex_num);
-  printf("Number with bytes swaped: %hx\n", swap_bytes(hex_num));
+  printf("Number with bytes swaped: %hx\n", swap_bytes_b(hex_num));
 
   return 0;
 }
@@ -20,4 +24,9 @@ unsigned short swap_bytes(unsigned short i)
   unsigned short b1_moved_right = i >> 8;
 
   return b0_moved_left | b1_moved_right;
+}
+
+unsigned short swap_bytes_b(unsigned short i)
+{
+  return i << 8 | i >> 8;
 }
