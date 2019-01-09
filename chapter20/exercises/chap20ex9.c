2afb779afa025b62ec41fa24428fd8953095c9de
TJRobson <thomas.j.robson@hotmail.com>
2019-01-09 14:08:00 +1300

    Exericse 9b done.

--- chapter20/exercises/chap20ex9.c
+++ chapter20/exercises/chap20ex9.c
@@ -5,16 +5,24 @@
 int count_ones(unsigned char ch);
 
 /* b) Write the function in part a) without using a loop. */
-// int count_ones_b(unsigned char ch);
+int count_ones_b(unsigned char ch);
 
 int main(void)
 {
-  unsigned char ch;
+  unsigned char ch_a, ch_b;
 
-  printf("\nEnter a unsigned char value: ");
-  scanf("%c", &ch);
+  printf("\nEnter a char value: ");
+  scanf("%c", &ch_a);
 
-  printf("# of 1's in char: %d\n", count_ones(ch));
+  printf("# of 1's in char: %d\n", count_ones(ch_a));
+
+  getchar(); /* discarding enter key char */
+
+  printf("Enter another char value: ");
+  scanf("%c", &ch_b);
+
+  printf("# of 1's in char: %d\n", count_ones_b(ch_b));
+
 
   return 0;
 }
@@ -30,4 +38,16 @@ int count_ones(unsigned char ch)
     }
   }
   return count;
+}
+
+int count_ones_b(unsigned char ch)
+{
+  return ((ch >> 7) & 1)
+        +((ch >> 6) & 1)
+        +((ch >> 5) & 1)
+        +((ch >> 4) & 1)
+        +((ch >> 3) & 1)
+        +((ch >> 2) & 1)
+        +((ch >> 1) & 1)
+        +(ch & 1);
 }
